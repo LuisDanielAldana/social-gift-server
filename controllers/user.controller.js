498e87913539efde3eb2ac2b2320e381c39b5216
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2023-05-16 19:00:45 +0200

    Modified routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -76,6 +76,107 @@ async function login(req, res){
     }
 }
 
+async function updateUser(req, res){
+    const _id = req.params.userId
+    const name = req.body.name
+    const lastname = req.body.lastname
+    const email = req.body.email
+    const password = req.body.password
+    try {
+        const updatedUser = await User.updateOne(
+            {_id:_id},
+            {
+                name: name,
+                lastname: lastname,
+                email: email,
+                password: password
+            }
+        )
+        res.status(200).json({
+            message: "Successfully updated User",
+            obj: updatedUser
+        })
+    } catch(e) {
+        res.status(400).json({
+            message: "Error updating user"
+        })
+    }
+}
+
+async function updateUserImage(req, res){
+    const _id = req.params.userId
+    const image = req.body.image
+    try {
+        const updatedUser = await User.updateOne(
+            {_id: _id},
+            {image: image}
+        )
+        res.status(200).json({
+            message: "Image updated successfully",
+            obj: updatedUser
+        })
+    } catch(e){
+        res.status(400).json({
+            message: "Unable to update image"
+        })
+    }
+}
+
+async function searchUser(req, res){
+    const search = req.body.search
+    var reg = new RegExp(`.*${search}.*`, 'i');
+    console.log(req.body)
+    try{
+        const users = await User.find().or([{ 'name': { $regex: reg }}, { 'lastName': { $regex: reg }}, { 'email': { $regex: reg }}])
+        res.status(200).json({
+            message: "All coincidences",
+            obj: users
+        })
+    } catch(e){
+        res.status(400).json({
+            message: "Error",
+            error: e
+        })
+    }
+}
+
+async function getFriends(req, res){
+    const _id = req.params.userId
+    try{
+        const friends = await User.findOne(
+            {_id:_id},
+            {friends:1}).populate({path:"friends", model:"User"})
+        res.status(200).json({
+            message: "All friends",
+            obj: friends
+        })
+    } catch(e){
+        res.status(400).json({
+            message: "Error"
+        })
+    }
+}
+
+async function sendFriendRequest(req, res){
+    const sender = req.params.userId
+    const receiver = req.params.receiver
+    try{
+        const reqReceiver = await User.updateOne(
+            {_id: receiver},
+            {$push:
+                    {friend_requests: sender}}
+        )
+        res.status(200).json({
+            message: "Request sent successfully",
+            obj: reqReceiver
+        })
+    } catch(e){
+        res.status(400).json({
+            message: "Error"
+        })
+    }
+}
+
 async function createWishlist(req, res) {
     const _id = req.params.userId
     const title = req.body.title
@@ -111,6 +212,11 @@ module.exports = {
     createUser,
     createWishlist,
     findUserById,
-    login
+    login,
+    updateUser,
+    updateUserImage,
+    searchUser,
+    getFriends,
+    sendFriendRequest
 }
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/user.controller')
+const {sendFriendRequest} = require("../controllers/user.controller");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,7 +12,13 @@ router.get('/', function(req, res, next) {
 router.post('/create', userController.createUser)
 router.get('/:userId', userController.findUserById)
 router.post('/login',userController.login)
+router.post('/:userId/update', userController.updateUser)
+router.put('/:userId/updateImage', userController.updateUserImage)
+router.post('/search',userController.searchUser)
+router.get('/:userId/friends', userController.getFriends)
+router.post('/:userId/friend-request/:receiver', sendFriendRequest)
 router.post('/:userId/create-wishlist', userController.createWishlist)
+
 
 
 module.exports = router;
